cb0db9b37724b929d19f5793ec07826ad4f1dbf3
magic9995 <wiigopher@gmail.com>
2021-07-31 13:52:00 -0400

    Fourth attempt cell Update

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -154,7 +154,7 @@ export default {
         if (sameDates(value, this.rowValue)) valueChanged = false
       }
       const { row, column, rowIndex, columnIndex } = this
-      if(valueChanged){
+      if (valueChanged) {
         this.$emit('edited', { row, column, rowIndex, columnIndex, $event, value, valueChanged })
       }
     },
